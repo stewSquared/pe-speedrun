ab2b12af3ce3c344462a7ea8fdb4d2dfc8bc61ae
Stewart Stewart <stewinsalot@gmail.com>
2021-03-09 20:31:13 -0500

    refactor `factors` into algs package

--- src/main/scala/algs.scala
+++ src/main/scala/algs.scala
@@ -1,5 +1,14 @@
 package object algs {
 
+  def factors(n: Long): List[Int] = {
+    def loop(n: Long, k: Int): List[Int] = {
+      if (n == 1) Nil
+      else if (n % k == 0) k :: loop(n/k, k)
+      else loop(n, k+1)
+    }
+    loop(n, k = 2)
+  }
+
   def primesUntil(n: Int): Seq[Int] = {
     val prime = Array.fill[Boolean](n)(true)
 
--- src/main/scala/p005.scala
+++ src/main/scala/p005.scala
@@ -1,17 +1,10 @@
 object p005 extends App {
-  def factors(n: Long): List[Int] = {
-    def loop(n: Long, k: Int): List[Int] = {
-      if (n == 1) Nil
-      else if (n % k == 0) k :: loop(n/k, k)
-      else loop(n, k+1)
-    }
-    loop(n, k = 2)
-  }
-
   val maxCounts = collection.mutable.Map.empty[Int, Int].withDefaultValue(0)
 
+  // TODO use prime generator for a rewrite
+
   (2 to 20).foreach { n =>
-    factors(n)
+    algs.factors(n)
       .groupBy(identity)
       .mapValues(_.length)
       .foreach { case (f, c) =>
