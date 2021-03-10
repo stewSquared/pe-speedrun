9addc11124098f0fd5f484edc5aac86a9a8de19d
Stewart Stewart <stewinsalot@gmail.com>
2021-03-09 19:14:21 -0500

    move sieve of erastothenes into new algs package

---
+++ src/main/scala/algs.scala
@@ -0,0 +1,32 @@
+package object algs {
+
+  def erastothenes: LazyList[Int] = {
+
+    def merge(left: LazyList[Int], right: LazyList[Int]): LazyList[Int] = {
+      (left, right) match {
+        case (lh #:: lt, rh #:: rt) =>
+          val next = lh min rh
+          next #:: merge(
+            if (next == lh) lt else left,
+            if (next == rh) rt else right
+          )
+        case _ => left #::: right
+      }
+    }
+
+    def mults(p: Int) = LazyList.from(p * p, p)
+
+    def loop(n: Int, primes: LazyList[Int], composites: LazyList[Int]): LazyList[Int] = {
+      (primes, composites) match {
+        case (_, c #:: cs) if n == c => loop(n+1, primes, cs)
+        case (p #:: ps, _) if n == p*p => loop(n, ps, merge(mults(p), composites))
+        case _ => n #:: loop(n+1, primes, composites)
+      }
+
+    }
+
+    lazy val primes: LazyList[Int] = 2 #:: loop(3, primes, LazyList.empty)
+    primes
+  }
+
+}
--- src/main/scala/p007.scala
+++ src/main/scala/p007.scala
@@ -1,35 +1,8 @@
 object p007 extends App {
 
-  def primes: LazyList[Int] = {
+  import algs.erastothenes
 
-    def merge(left: LazyList[Int], right: LazyList[Int]): LazyList[Int] = {
-      (left, right) match {
-        case (lh #:: lt, rh #:: rt) =>
-          val next = lh min rh
-          next #:: merge(
-            if (next == lh) lt else left,
-            if (next == rh) rt else right
-          )
-        case _ => left #::: right
-      }
-    }
-
-    def mults(p: Int) = LazyList.from(p * p, p)
-
-    def loop(n: Int, primes: LazyList[Int], composites: LazyList[Int]): LazyList[Int] = {
-      (primes, composites) match {
-        case (_, c #:: cs) if n == c => loop(n+1, primes, cs)
-        case (p #:: ps, _) if n == p*p => loop(n, ps, merge(mults(p), composites))
-        case _ => n #:: loop(n+1, primes, composites)
-      }
-
-    }
-
-    lazy val p: LazyList[Int] = 2 #:: loop(3, p, LazyList.empty)
-    p
-  }
-
-  val ans = primes(10000)
+  val ans = erastothenes(10000)
 
   println(ans)
 }
