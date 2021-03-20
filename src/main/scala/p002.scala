6d023cb7ae79448cd79d0f0425506703369d2d4f
Stewart Stewart <stewinsalot@gmail.com>
2021-03-20 12:40:02 -0400

    Use sequences.fibonacci in problems 2 and 25

--- src/main/scala/p002.scala
+++ src/main/scala/p002.scala
@@ -1,11 +1,8 @@
+import sequences.fibonacci
+
 object p002 extends App {
-  def fibs: LazyList[Int] = {
-    def loop(a: Int, b: Int): LazyList[Int] = a #:: loop(b, a + b)
 
-    loop(0, 1)
-  }
-
-  val ans = fibs
+  val ans = fibonacci
     .filter(_ % 2 == 0)
     .takeWhile(_ < 4000000)
     .sum
--- src/main/scala/p025.scala
+++ src/main/scala/p025.scala
@@ -1,13 +1,8 @@
+import sequences.fibonacci
+
 object p025 extends App {
 
-  def fibs: LazyList[BigInt] = {
-    def loop(a: BigInt, b: BigInt): LazyList[BigInt] =
-      a #:: loop(b, a + b)
-
-    loop(BigInt(0), BigInt(1))
-  }
-
-  val ans = fibs.indexWhere(f => f.toString.length == 1000)
+  val ans = fibonacci.indexWhere(f => f.toString.length == 1000)
 
   println(ans)
 }
