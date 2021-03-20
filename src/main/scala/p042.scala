9c79985a0980cbe29cc0a9127904e8600f363080
Stewart Stewart <stewinsalot@gmail.com>
2021-03-20 12:42:24 -0400

    Use sequences to simplify 12, 42, 45
    - Simplify perfectSquare in 12, import triangular
    - Use isTriangular in 42
    - Simplify 45 using hexagonals/isPentagonal

--- src/main/scala/p042.scala
+++ src/main/scala/p042.scala
@@ -1,10 +1,6 @@
+import sequences.isTriangular
+
 object p042 extends App {
-  def triangleNumber(n: Int): Int = n * (n+1) / 2
-
-  val triangleNumbers = LazyList.from(1).map(triangleNumber)
-
-  def isTriangular(n: Int): Boolean =
-    triangleNumbers.takeWhile(_ <= n).contains(n)
 
   def alphabeticalValue(s: String): Int = s.map(_ - 64).sum
 
--- src/main/scala/p045.scala
+++ src/main/scala/p045.scala
@@ -1,26 +1,10 @@
+import sequences.{hexagonals, isPentagonal}
+
 object p045 extends App {
-
-  def pentagonalNumber(n: Long): Long = n * (3*n - 1) / 2
-
-  def hexagonalNumber(n: Long): Long = n * (2*n - 1)
 
   // note that all hexagonals are already triangle numbers
 
-  // def merge(pents: LazyList[Int], hexes: LazyList[Int]): LazyList[Int] = {
-  //   (pents, hexes) match {
-  //     case (p#::ps, h#::_) if p < h => merge(ps, hexes)
-  //     case (p#::_, h#::hs) if p > h => merge(pents, hs)
-  //     case (p#::ps, h#::hs) if p == h => p #:: merge(ps, hs)
-  //   }
-  // }
-
-  def isPentagonal(n: Long): Boolean = (1 + math.sqrt(1 + 24*n)) % 6 == 0.0
-
-  val allThree = LazyList.from(1)
-    .map(n => hexagonalNumber(n.toLong))
-    .filter(isPentagonal)
-
-  val ans = allThree.drop(2).head
+  val ans = hexagonals.filter(isPentagonal).drop(2).next
 
   println(ans)
 }
