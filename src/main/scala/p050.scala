3dca999cbeeea8eb6dce95187baa8820695e2d80
Stewart Stewart <stewinsalot@gmail.com>
2021-03-27 12:54:17 -0400

    Refactor/optimize problems 50

--- src/main/scala/p050.scala
+++ src/main/scala/p050.scala
@@ -1,33 +1,25 @@
+import algs.primesUntil
+
 object p050 extends App {
-  val primes = algs.primesUntil(1_000_000).toVector
+  val primes = primesUntil(1_000_000).toVector
 
-  def prefixes = primes.scanLeft(0)(_ + _)
+  val isPrime = primes.toSet
 
-  def sumOfSlice(i: Int, j: Int): Int = {
-    prefixes(j) - prefixes(i)
-  }
+  val consecutiveUpperLimit = primes
+    .scanLeft(0)(_ + _)
+    .takeWhile(_ <= primes.last)
+    .length
 
-  assert(sumOfSlice(0,1) == 2, "optimized slice")
-  assert(primes.slice(0,1).sum == 2, "built in slice")
+  def primeSum(consecutive: Int): Option[Int] = primes
+    .sliding(consecutive).toSeq
+    .map(_.sum)
+    .takeWhile(_ <= primes.last)
+    .find(isPrime)
 
-  def longestSum(target: Int, index: Int): Option[Seq[Int]] = {
-    def loop(i: Int, j: Int): Option[Seq[Int]] = {
-      val sum = sumOfSlice(i, j)
-      if (j == index) None
-      else if (sum == target) Some(primes.slice(i, j))
-      else if(sum > target) loop(i+1, j)
-      else loop(i, j+1)
-    }
+  def largestPrime(limit: Int): Int =
+    primeSum(limit).getOrElse(largestPrime(limit - 1))
 
-    loop(0,1).filter(_.length > 1)
-  }
-
-  // TODO: be smarter about bounds for searching for optimization
-  val primesToCheck = primes.zipWithIndex.reverse.take(10000)
-
-  val sums = primesToCheck.flatMap { case (p, i) => longestSum(p, i) }
-
-  val ans = sums.maxBy[Int](_.length).sum
+  val ans = largestPrime(consecutiveUpperLimit)
 
   println(ans)
 }
