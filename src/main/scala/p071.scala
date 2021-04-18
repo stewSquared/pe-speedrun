abe51937f4a73f3aaf9e017180d2f35259ac155d
Stewart Stewart <stewinsalot@gmail.com>
2021-04-18 19:53:18 -0400

    Commit problems 70-74

---
+++ src/main/scala/p071.scala
@@ -0,0 +1,14 @@
+import fractions.Fraction
+import fractions.Fraction.over
+
+@main def p071(): Unit = {
+  val fractions = for {
+    d <- 1 to 1000000
+    if d%7 != 0
+    n = d * 3 / 7
+  } yield Fraction(n, d)
+
+  val ans = fractions.max
+
+  println(ans.numerator)
+}
---
+++ src/main/scala/p073.scala
@@ -0,0 +1,17 @@
+import fractions.Fraction
+import fractions.Fraction.over
+
+// TODO: 42 seconds. optimize
+
+@main def p073(): Unit = {
+  val fractions = for {
+    d <- 1 to 12000
+    n0 = (d / 3) + 1
+    n1 = if (d%2 != 0) d / 2 else d / 2 - 1
+    n <- n0 to n1
+  } yield Fraction(n, d)
+
+  val ans = fractions.distinct.length
+
+  println(ans)
+}
